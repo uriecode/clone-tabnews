6eebdead7ee8bebbb2fb34616522d250fd46e5ee
Beatriz Barbosa <90875963+uriecode@users.noreply.github.com>
2025-12-08 13:05:04 +0000

    adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <div>
+            <h1>EI CARA...</h1>
+            <img src="https://gartic.com.br/imgs/mural/i_/i_broken_/pipoca-na-manteiga.png"/>
+            <p>você gosta de pipoca?</p>
+            <p>
+                pipoca... <strong><em>QUENTE NA MANTEIGA?</em></strong>
+            </p>
+        </div>
+    );
+}
+
+export default Home;
